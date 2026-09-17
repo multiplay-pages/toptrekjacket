3951e4df119759303ace0af2ce0d37f6d8ea4e3e
multiplay-pages <trolopiszek@gmail.com>
2026-09-17 12:23:15 +0200

    Stage 8: load ranking UX overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './stage3.css'
 import './stage7.css'
+import './stage8.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
